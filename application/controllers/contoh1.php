e1f3f9f1f2d73b93c7ba3910937c1258a085d23b
knabilv <knabilv.kv@gmail.com>
2023-03-30 08:42:07 +0700

    Menambahkan Contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,16 @@
+<?php
+  class Contoh1 extends CI_Controller
+  {
+
+    public function index()
+    {
+      echo "<h1>Perkenalan</h1>";
+      echo "Nama saya Muhammad Nabil<br>
+            Dengan NIM 17190202<br>
+            Kelas 17.6A.01<br>
+            Tinggal di daerah Depok<br>
+            Olahraga yang saya sukai adalah E-Sport
+            ";
+    }
+  }
+?>
